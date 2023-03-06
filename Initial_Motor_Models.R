4afceac158a945f6b5531fcb05d7dc5c37df3dcd
Anthony Crespo <anthonycrespo@hotmail.com>
2023-03-05 21:15:36 -0500

    Initial lm of torque as a function of everything

--- Initial_Motor_Models.R
+++ Initial_Motor_Models.R
@@ -8,14 +8,14 @@ library(factoextra)
 
 df <- motor_cleaned_beck
 df <- df[-1]
+df$profile_id <- as.factor(df$profile_id)
+
 
 ###PCA###
 pc <- prcomp(df[-13],center = TRUE, scale = TRUE)
 
 fviz_eig(pc)#scree plot of PCs
-
-fviz_pca_biplot(pc, repel = TRUE, #Viz of PCA
-                col.var = "red")
+fviz_pca_var(pc)#vector plot of main contributors to pc1 and pc2
 
 
 eig.val <- get_eigenvalue(pc) 
@@ -26,11 +26,22 @@ pc_var$contrib #contributions to the PCs
 
 
 #Subset data into test and train
-
-
-#PCR 
 set.seed(100)
-pcrTune <- train(x = solTrainXtrans, y = solTrainY, method = "pcr", tuneGrid = expand.grid(ncomp = 1:35), trControl = ctrl)
-pcrTune 
+sample <- sample(c(TRUE, FALSE), nrow(df), replace=TRUE, prob=c(0.8,0.2))
+train  <- df[sample, ]
+test   <- df[!sample, ]
+
+#torque as a function of everything
+lmTorqueAllPredictors <- lm(torque ~ . , data = train)
+summary(lmTorqueAllPredictors)
+
+#prediction power of torque as a function of everything
+PredTorque = predict(lmTorqueAllPredictors, test)
+#prediction results show over 99% accuracy
+lmValues1 = data.frame(obs = test$torque, pred = PredTorque)
+defaultSummary(lmValues1)
+
+
+
 
 
